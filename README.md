e1e05ac9f7f6711bc9c5d779bc71d39407063988
supra126 <supra126@gmail.com>
2023-04-20 13:27:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# @simoko/tw-zip
+
+[![NPM version](https://img.shields.io/npm/v/@simoko/tw-zip?color=a1b858&label=)](https://www.npmjs.com/package/@simoko/tw-zip)
+
+- 台灣 **縣市**、**行政區**、**郵遞區號**（3碼）資料
+- 可直接使用 data
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import data from './data'
+
+interface IDistrict {
+  [key: string]: string
+}
+
+interface ICity {
+  [key: string]: IDistrict
+}
+
+/**
+ * 回傳所有資料
+ * @example
+ * {
+ *   "台北市": {
+ *     "中正區": "100",
+ *     "大同區": "103",
+ *     ...
+ *   },
+ *   "基隆市": {
+ *     "仁愛區": "200",
+ *     "信義區": "201",
+ *     ...
+ *   },
+ *   "新北市": {
+ *     "萬里區": "207",
+ *     "金山區": "208",
+ *     ...
+ *   },
+ *   ...
+ * }
+ */
+function getData(): ICity {
+  return data
+}
+
+/**
+ * 回傳縣市陣列
+ * @example ['台北市', '基隆市', '新北市', ...]
+ */
+function getCityArray(): string[] {
+  return Object.keys(data)
+}
+
+/**
+ * 回傳行政區資料
+ * @param city 縣市名稱
+ * @example { "中正區": "100", "大同區": "103", ... }
+*/
+function getDistricts(city: string): IDistrict {
+  return Object.entries(data).filter(x => x[0] === city).flatMap(([k1, v1]) => v1)[0] || ({} as IDistrict)
+}
+
+/**
+ * 回傳扁平化資料
+ * @param city 縣市名稱 (可選，預設回傳所有縣市)
+ * @param symbol 分隔符號 (可選，預設為`空格符`)
+ * @example
+ * getFlatArray({ city: '嘉義市', symbol: '#' })
+ * // return [ '600#嘉義市#東區', '600#嘉義市#西區' ]
+ * @example
+ * getFlatArray()
+ * // return ['100 台北市 中正區', '103 台北市 大同區', ...]
+*/
+function getFlatArray({ city = '', symbol = ' ' } = {}): string[] {
+  return Object.entries(data).filter(x => city ? x[0] === city : true).flatMap(([k1, v1]) => Object.entries(v1).flatMap(([k2, v2]) => `${v2}${symbol}${k1}${symbol}${k2}`))
+}
+
+export default { getData, getCityArray, getDistricts, getFlatArray }
